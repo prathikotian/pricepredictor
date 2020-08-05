1f125d05023971be171e2d4e5baf1e44b5e1ad23
prathikotian <prathi.kotian@outlook.com>
2020-08-06 02:32:37 +0530

    cosmetic changes

--- price.js
+++ price.js
@@ -1,13 +1,27 @@
-var btn = document.querySelector('#btn');
-var bitprice = document.querySelector('#bitprice');
+const api = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+let btn = document.querySelector('#btn');
+let bitprice = document.querySelector('#bitprice');
+
+window.onload = () => {
+	let XHR = new XMLHttpRequest();
+	XHR.open('GET', api);
+	XHR.onreadystatechange = function () {
+		if(XHR.readyState === 4 && XHR.status === 200) {
+			let response = XHR.responseText;
+			let parsedResponse = JSON.parse(response);
+			bitprice.innerText = parsedResponse.bpi.USD.rate + ' ' + parsedResponse.bpi.USD.code;
+		}
+	}
+	XHR.send();	
+};
 
 btn.addEventListener('click', function(){
-	var XHR = new XMLHttpRequest();
-	XHR.open('GET', 'https://api.coindesk.com/v1/bpi/currentprice.json');
+	let XHR = new XMLHttpRequest();
+	XHR.open('GET', api);
 	XHR.onreadystatechange = function(){
 		if(XHR.readyState === 4 && XHR.status === 200) {
-			var response = XHR.responseText;
-			var parsedResponse = JSON.parse(response);
+			let response = XHR.responseText;
+			let parsedResponse = JSON.parse(response);
 			bitprice.innerText = parsedResponse.bpi.USD.rate + ' ' + parsedResponse.bpi.USD.code;
 		}
 	}
